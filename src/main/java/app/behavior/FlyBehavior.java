5f811c9dd932204a260515423584f2f0b0513476
Aleksandr Petrov <petrov.aleksandr0412@gmail.com>
2020-02-04 14:15:52 +0300

    init

---
+++ src/main/java/app/behavior/FlyBehavior.java
@@ -0,0 +1,17 @@
+package app.behavior;
+
+public class FlyBehavior {
+    private String fly;
+
+    public FlyBehavior(String fly) {
+        this.fly = fly;
+    }
+
+    public String getFly() {
+        return fly;
+    }
+
+    public void setFly(String fly) {
+        this.fly = fly;
+    }
+}
